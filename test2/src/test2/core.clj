7f099cf01ed01d5b692a1395c0d5aef2a7c20263
Shiva Krishnamurthy <jaishrik@acm.org>
2013-02-09 13:06:08 -0500

    initial add
    testing

---
+++ test2/src/test2/core.clj
@@ -0,0 +1,7 @@
+(ns test2.core
+   (:gen-class))
+
+(defn -main
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test2/test/test2/core_test.clj
@@ -0,0 +1,7 @@
+(ns test2.core-test
+  (:use clojure.test
+        test2.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
